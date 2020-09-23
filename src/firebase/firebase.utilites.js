10943885844365092b80160e038e04fcddf1abce
asaduzzaman69 <66587375+asaduzzaman69@users.noreply.github.com>
2020-09-23 16:56:08 +0600

    Update firebase.utilites.js

--- src/firebase/firebase.utilites.js
+++ src/firebase/firebase.utilites.js
@@ -3,14 +3,7 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const FirebaseConfig = {
-    apiKey: "AIzaSyBU-9vIjsIZqZe-m3koazzxSy0G7J0NQQA",
-    authDomain: "todo-app-3d55f.firebaseapp.com",
-    databaseURL: "https://todo-app-3d55f.firebaseio.com",
-    projectId: "todo-app-3d55f",
-    storageBucket: "todo-app-3d55f.appspot.com",
-    messagingSenderId: "42111802593",
-    appId: "1:42111802593:web:ebd6dfdda7e845aeee2ba0",
-    measurementId: "G-KCNPXP8EFF"
+   //your config plz
 }
 firebase.initializeApp(FirebaseConfig);
 
